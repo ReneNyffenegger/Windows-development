eaecae9d10aa5c08045e2d38447929de4a38992d
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-02-11 09:39:07 +0100

    print environment variables

---
+++ WOW64/environment-variables/call-print-env-vars.bat
@@ -0,0 +1,2 @@
+call                              print-env-vars.bat
+%systemroot%\syswow64\cmd.exe /c "print-env-vars.bat"
---
+++ WOW64/environment-variables/print-env-vars.bat
@@ -0,0 +1,8 @@
+@echo off
+
+echo PROCESSOR_ARCHITECTURE  = %PROCESSOR_ARCHITECTURE%
+echo PROCESSOR_ARCHITEW6432  = %PROCESSOR_ARCHITEW6432%
+echo ProgramFiles            = %ProgramFiles%
+echo ProgramW6432            = %ProgramW6432%
+echo CommonProgramFiles      = %CommonProgramFiles%
+echo CommonProgramW6432      = %CommonProgramW6432%
